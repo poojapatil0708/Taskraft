5b153cadce322b315060f2fd2f6cc2c29a36f55b
Pooja Patil <poojapatil0708@gmail.com>
2024-03-21 14:20:15 +0530

    added prod url

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,6 +1,6 @@
 const constants = {
-    base_url_local: 'http://localhost:8000',
-    base_url_production: 'https://todo-list-server-cho1.onrender.com'
+    base_url_local: 'https://taskraft.onrender.com',
+    base_url_production: 'https://taskraft.onrender.com'
 }
 
 export default constants;
